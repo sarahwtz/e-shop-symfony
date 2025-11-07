be527c833e35bdeff7f57ee4b24767434bda4109
Sarah Wentzel <sarahwtz@gmail.com>
2025-11-07 23:22:19 +0100

    Add create and persist for Category and Product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,20 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use App\Service\UploaderHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class ProductController extends AbstractController
 {
     #[Route('/admin/view/products', name: 'app_product')]
     public function index(ProductRepository $productRepository): Response
     {
-
         $products = $productRepository->findAll();
         return $this->render('admin/product/index.html.twig', [
            'products' => $products,
+        ]);
+    }
+
+    #[Route('/admin/add/product', name: 'app_product_add')]
+    public function new(Request $request, EntityManagerInterface $entityManager, UploaderHelper $uploaderHelper): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product->setCreatedAt(new \DateTimeImmutable());
+
+            $uploadedFile = $form['image']->getData();
+            if ($uploadedFile) {
+                $newFilename = $uploaderHelper->uploadProductImage($uploadedFile);
+                $product->setImage($newFilename);
+            }
+
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_product');
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Service/UploaderHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploaderHelper
+{
+    private string $uploadsPath;
+    private SluggerInterface $slugger;
+
+    public function __construct(string $uploadsPath, SluggerInterface $slugger)
+    {
+        $this->uploadsPath = $uploadsPath;
+        $this->slugger = $slugger;
+    }
+
+    public function uploadProductImage(UploadedFile $uploadedFile): string
+    {
+        $destination = $this->uploadsPath.'/product_images';
+
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+
+        $newFilename = $safeFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+
+        $uploadedFile->move($destination, $newFilename);
+
+        return $newFilename;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->uploadsPath.'/product_images';
+    }
+}
